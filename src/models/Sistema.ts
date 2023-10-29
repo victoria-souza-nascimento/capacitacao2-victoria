85a89f128734b7b048a77a48800a7806332aa7c7
Victoria Souza Nascimento <vicsouza19@icloud.com>
2023-10-29 20:20:26 -0300

    commit inicial

---
+++ src/models/Sistema.ts
@@ -0,0 +1,48 @@
+export class Sistema {
+    private BancoDeDados: string
+    constructor(BancoDeDados: string) {
+        this.BancoDeDados = BancoDeDados
+
+
+    }
+    logar(): string {
+        return this.BancoDeDados
+
+
+    }
+
+
+}
+export class BancoDeDados {
+    usuarios: string
+    constructor(usuarios: string) {
+this.usuarios = usuarios
+
+    }
+    obterusuarios(): string {
+        return this.usuarios
+
+    }
+    salvarusuarios(): string {
+        return this.usuarios
+    }
+    editarusuarios(): string {
+        return this.usuarios
+    }
+    deletarusuarios(): string {
+        return this.usuarios
+
+    }
+        
+
+
+}
+ {
+        this.nome = this.nome
+        this.senha = this.senha
+    }
+
+
+
+
+}
